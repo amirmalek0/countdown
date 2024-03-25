592e91b5fff8b3a3c56842c142dbfa19df94cc37
Amir Malek <amirhoseynmalekzadeh@gmail.com>
2024-03-26 03:17:40 +0330

    Controls for countdown added.(Pause,Resume,StarOver)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+isPaused = false;
+
 function count() {
     let startingHours = document.getElementById('countdownHour').valueAsNumber;
     let startingMinutes = document.getElementById('countdownMin').valueAsNumber;
@@ -13,32 +15,45 @@ function count() {
     }
     globalThis.time = (startingHours * 60 * 60) + (startingMinutes * 60) + startingSeconds;
     setInterval(timer, 1000);
+}
 
+function pause() {
+    isPaused = true;
+}
+
+function resume() {
+    isPaused = false;
+}
+
+function startOver() {
+   location.reload();
 }
 
 function timer() {
-    let hourDiv = document.getElementById("hour");
-    let minDiv = document.getElementById("min");
-    let secDiv = document.getElementById("sec");
+    if (isPaused === true) {
+    } else if (isPaused === false) {
+            let hourDiv = document.getElementById("hour");
+            let minDiv = document.getElementById("min");
+            let secDiv = document.getElementById("sec");
 
-    let hour = Math.floor(time / 3600);
-    let minute = Math.floor((time % 3600) / 60);
-    let second = (time % 3600) % 60;
-    if (second === 0) {
-        second = "" + second + second;
-    }
-    if (hour === 0 && minute === 0 && second === '00') {
-        hourDiv.innerHTML = hour;
-        minDiv.innerHTML = minute;
-        secDiv.innerHTML = second;
-        return;
-    }
-    console.log(hourDiv)
-    hourDiv.innerHTML = hour;
-    minDiv.innerHTML = minute;
-    secDiv.innerHTML = second;
-    document.getElementById('timer').style.display='block';
-    document.getElementById('counter-inputs').style.display='none';
+            let hour = Math.floor(time / 3600);
+            let minute = Math.floor((time % 3600) / 60);
+            let second = (time % 3600) % 60;
+            if (second === 0) {
+                second = "" + second + second;
+            }
+            if (hour === 0 && minute === 0 && second === '00') {
+                hourDiv.innerHTML = hour;
+                minDiv.innerHTML = minute;
+                secDiv.innerHTML = second;
+                return;
+            }
+            hourDiv.innerHTML = hour;
+            minDiv.innerHTML = minute;
+            secDiv.innerHTML = second;
+            document.getElementById('timer').style.display = 'block';
+            document.getElementById('counter-inputs').style.display = 'none';
 
-    time--;
+            time--;
+    }
 }
